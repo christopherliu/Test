4cfde3c5d6d4e030e37eee39cbbb06b8ab45766a
Christopher Liu <github@christopherliu.net>
2022-06-28 15:22:32 -0400

    First commit

---
+++ testlib/dist/components/component-blocks.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.componentBlocks = void 0;
+function componentBlocks() {
+}
+exports.componentBlocks = componentBlocks;
---
+++ testlib/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.constant = void 0;
+exports.constant = 5;
